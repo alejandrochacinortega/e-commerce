a2d26ed2e69a715ad31f07ba9c038e66c171966b
alejandroortega <alejandro.chacin.ortega@gmail.com>
2024-05-10 11:45:07 +0200

    add header, dark mode

---
+++ app/data.ts
@@ -0,0 +1,94 @@
+import { faker } from "@faker-js/faker";
+
+interface IUser {
+  userId: string;
+  username: string;
+  email: string;
+  avatar: string;
+  password: string;
+  birthdate: Date;
+  registeredAt: Date;
+}
+
+interface Product {
+  id: string;
+  category: string;
+  name: string;
+  price: string;
+  images: string[];
+}
+
+export function createRandomUser(): IUser {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+export const USERS: IUser[] = faker.helpers.multiple(createRandomUser, {
+  count: 5,
+});
+
+export const PRODUCTS = [
+  {
+    id: "1",
+    category: "Camera",
+    name: "Sony FX3",
+    price: "$3,999.00",
+    images: ["/img/products/FX3.png"],
+  },
+  {
+    id: "2",
+    category: "Camera",
+    name: "Sony A7S III",
+    price: "$3,499.00",
+    images: ["/img/products/7SIII.png"],
+  },
+  {
+    id: "3",
+    category: "Camera",
+    name: "Sony A7C",
+    price: "$1,599.00",
+    images: ["/img/products/7C.png"],
+  },
+  {
+    id: "4",
+    category: "Camera",
+    name: "Sony A7 IV",
+    price: "$2,399.00",
+    images: ["/img/products/7IV.png"],
+  },
+  {
+    id: "5",
+    category: "Camera",
+    name: "Sony A7R III",
+    price: "$2,499.00",
+    images: ["/img/products/7RIII.png"],
+  },
+  {
+    id: "6",
+    category: "Camera",
+    name: "Sony A7R V",
+    price: "$3,899.00",
+    images: ["/img/products/7RV.png"],
+  },
+  {
+    id: "7",
+    category: "Camera",
+    name: "Sony A6700",
+    price: "$1,799.00",
+    images: ["/img/products/6700.png"],
+  },
+  {
+    id: "8",
+    category: "Camera",
+    name: "Sony AZV-E10",
+    price: "$699.00",
+    images: ["/img/products/ZVE10.png"],
+  },
+];
